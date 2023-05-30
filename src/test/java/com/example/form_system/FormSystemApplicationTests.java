1c6892d33d07003a03a361c4106b981041a7b0cd
Kouhei <alan829221@gmail.com>
2023-05-31 03:04:08 +0800

    INIT: backend

---
+++ src/test/java/com/example/form_system/FormSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.form_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
